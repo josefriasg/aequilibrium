cf5fb24444652d26308551ad0003b9644e777cd4
Jose Frias <joseduardofrias@gmail.com>
2020-12-21 13:03:23 -0500

    Fill dto information

--- src/main/java/com/aequilibrium/transformers/dto/BattleResponseDto.java
+++ src/main/java/com/aequilibrium/transformers/dto/BattleResponseDto.java
@@ -1,5 +1,21 @@
 package com.aequilibrium.transformers.dto;
 
+import java.util.List;
+
+import com.aequilibrium.transformers.model.Transformer;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor
 public class BattleResponseDto {
+	
+private Integer numBattles;
+	
+	private String winningTeam;
+	
+	private List<Transformer> survivors;
 
 }
