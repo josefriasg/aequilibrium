f221fd0b341c212fc65447760a090ecbe582768f
Jose Frias <joseduardofrias@gmail.com>
2020-12-21 13:00:20 -0500

    Initialize database with some default data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+ INSERT INTO TRANSFORMER VALUES ((SELECT nextval('hibernate_sequence')), 10, 10, 10, 10, 'Optimus Prime', 1, 10, 10, 10, 'A' );
+ INSERT INTO TRANSFORMER VALUES ((SELECT nextval('hibernate_sequence')), 9, 8, 7, 6, 'Bumblebee', 2, 7, 8, 9, 'A' );
+ INSERT INTO TRANSFORMER VALUES ((SELECT nextval('hibernate_sequence')), 8, 6, 4, 7, 'Bluestreak', 3, 6, 8, 4, 'A' );
+ INSERT INTO TRANSFORMER VALUES ((SELECT nextval('hibernate_sequence')), 10, 10, 10, 10, 'Predaking', 1, 10, 10, 10, 'D' );
+ INSERT INTO TRANSFORMER VALUES ((SELECT nextval('hibernate_sequence')), 7, 4, 5, 6, 'Megatron', 2, 5, 7, 6, 'D' );
+ INSERT INTO TRANSFORMER VALUES ((SELECT nextval('hibernate_sequence')), 8, 5, 6, 5, 'Soundwave', 3, 4, 5, 3, 'D' );
