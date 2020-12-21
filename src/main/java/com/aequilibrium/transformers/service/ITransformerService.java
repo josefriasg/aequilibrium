d003e78d7e469554883c300f133cf14324101db3
Jose Frias <joseduardofrias@gmail.com>
2020-12-21 11:13:22 -0500

    Externalize strings

--- src/main/java/com/aequilibrium/transformers/service/ITransformerService.java
+++ src/main/java/com/aequilibrium/transformers/service/ITransformerService.java
@@ -7,6 +7,15 @@ import com.aequilibrium.transformers.model.BattleResponse;
 import com.aequilibrium.transformers.model.Transformer;
 
 public interface ITransformerService {
+	
+	public static final String WINNING_TEAM_AUTOBOTS="Autobots";
+	public static final String WINNING_TEAM_DECEPTICONS="Decepticons";
+	public static final String WINNING_TEAM_NONE="None";
+	
+	public static final String OPTIMUS_PRIME="Optimus Prime";
+	public static final String PREDAKING="Predaking";
+	
+	
 	public Transformer createTransformer (Transformer transformer);
 	public Transformer updateTransformer (Transformer transformer);
 	public List<Transformer> getAllTransformer();
